542d4ce0cb6da6d41c514cedd16e70e270e7da28
JuanPorta14 <juanporta.c@gmail.com>
2024-10-30 00:02:28 -0300

    commit de seguridad

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,21 +4,24 @@ import { AppService } from './app.service';
 import { EvaController } from './eva/eva.controller';
 import { EvaService } from './services/eva/eva.service';
 import { User } from './entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbmanagerService } from './services/dbmanager/dbmanager.service';
 
-const dbPassword = process.env.DBPASSWORD
+const dbPassword = process.env.DBPASSWORD;
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: '34.42.167.179',
-    port: 3306,
-    username: 'root',
-    password: dbPassword,
-    database: 'Eva',
-    entities: [User],
-    synchronize: true,
-  })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '34.42.167.179',
+      port: 3306,
+      username: 'root',
+      password: dbPassword,
+      database: 'Eva',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, EvaController],
-  providers: [AppService, EvaService],
+  providers: [AppService, EvaService, DbmanagerService],
 })
 export class AppModule {}
